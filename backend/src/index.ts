59e5d12ae2b84f36342e19e05c304401b69dc01f
hriteshransingh <hriteshransing67220@gmail.com>
2026-04-18 09:38:04 +0530

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import {ENV} from "./config/env";
+import cors from "cors";
+
+import { clerkMiddleware } from '@clerk/express';
+
+
+const app = express();
+
+app.use(cors({origin: ENV.FRONTEND_URL})); 
+app.use(clerkMiddleware()); // auth object will be attached to the object
+app.use(express.json());  //parses json requested bodies
+app.use(express.urlencoded({extended: true})); // parses form data like html forms
+
+
+
+app.get("/", (req, res) => {
+
+    const {title, imgURL, desc} = req.body;
+    res.json({
+        message: "Welcome to Productify API",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments"
+
+        }
+       
+    })
+});
+
+
+app.listen(ENV.PORT, ()=> {
+    console.log(`App is running on PORT: ${ENV.PORT}`);
+    
+   
+});
